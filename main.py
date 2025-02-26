f198739a525cf3df7e61cb62264453be702e013e
Smite Chow <xiaopengyou@live.com>
2025-02-26 03:40:31 +0000

    fix access attr on dict

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from PIL import Image
 from transformers import pipeline
 from argparse import ArgumentParser
+import io
 
 import requests
 
 
-def main(mod_api, nsfw_api, report_api):
+def main(mod_api, nsfw_api, report_api, dev):
     response = requests.get(mod_api)
     posts = response.json()
 
-    if not posts.mod:
+    if not posts['mod']:
+        print(f'no post mod')
         return
 
     did_images = {}
-    for post in posts.mod:
-        did_images.setdefault(post.author, [])
-        did_images[post.author].extend(post.imgUrls.split(','))
+    for post in posts['mod']:
+        # only handle 3 author when dev
+        if dev and len(did_images) >=3:
+            break
+        author = post['author']
+        did_images.setdefault(author, [])
+        did_images[author].extend(post['imgUrls'].split(','))
 
     did_categories = []
     for did, imgs in did_images.items():
+        print(f'category did: {did} by analysis {len(imgs)} images')
         category = 0
 
         for img in imgs:
@@ -39,9 +46,11 @@ def main(mod_api, nsfw_api, report_api):
         did_categories.append({'category': category, 'did': did})
 
     response = requests.post(nsfw_api, json={'categories': did_categories, 'move': True})
+    data = response.json()
+    print(f'send mod result, {data["message"]}')
 
 
-def check_nsfw(data)
+def check_nsfw(data):
     img = Image.open(data)
     classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
     result = classifier(img)
@@ -55,6 +64,6 @@ if __name__ == '__main__':
     parser.add_argument("--get-mod-api", help="get mod endpoint", default='http://localhost:3000/xrpc/com.hukoubook.fg.getModImagePosts')
     parser.add_argument("--update-nsfw-api", help="update nsfw category endpoint", default='http://localhost:3000/xrpc/com.hukoubook.fg.updateNSFW')
     parser.add_argument("--get-report-nsfw-api", help="get report nsfw endpoint", default='n/a')
+    parser.add_argument("--dev", action="store_true")
     args = parser.parse_args()
-    main(args.get_mod_api, args.update_nsfw_api, args.get_report_nsfw_api)
-    
+    main(args.get_mod_api, args.update_nsfw_api, args.get_report_nsfw_api, args.dev)
